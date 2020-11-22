45c664e96553c1487d33a2dd6195fac4ffb900a9
eby8zevin <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2020-11-22 17:19:19 +0700

    Latihan PHP I

---
+++ PHP/Latihan/php_dojo_1/page1/index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo 'Hello, PHP';
+
+echo '<br>';
+
+echo '10 + 7';
+
+// Tanpa meletakkan 10 + 7 didalam tanda kutip,
+// kalkulasi numerik akan dilakukan seperti yang ditunjukkan di bawah:
+// echo 10 + 7;
+// Hasil => 17
+
+?>
---
+++ PHP/Latihan/php_dojo_1/page2/index.php
@@ -0,0 +1,14 @@
+<?php
+$name = 'Tom';
+echo 'Nilai $name: '.$name;
+echo '<br>';
+echo '-----';
+echo '<br>';
+
+// Ketik code Anda dibawah
+echo 'Nama saya adalah '.$name;
+
+// Solusi berbeda adalah dengan menggunakan substitusi variable
+// echo "Nama saya adalah {$name}";
+
+?>
