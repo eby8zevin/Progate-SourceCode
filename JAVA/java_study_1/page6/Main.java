ab9baa8a7eeedc0bec6775df070d3586674a453f
eby8zevin <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2020-12-01 05:54:26 +0700

    Java I

---
+++ JAVA/java_study_1/page6/Main.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) {
+    // Deklarasikan variable number dengan tipe int
+    
+    
+    // Tetapkan 3 ke variable number
+    int number = 3;
+    
+    // Cetak variable number
+    System.out.println(number);
+    
+    // Deklarasikan variable name dengan tipe String
+    
+    
+    // Tetapkan "Bob" ke variable name 
+    String name = "Bob";
+    
+    // Cetak variable name 
+    System.out.println(name);
+    
+    
+  }
+}
