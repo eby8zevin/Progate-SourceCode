71cf5c1daba5a803e1883780141d8de38929ee44
eby8zevin <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2020-12-03 05:27:37 +0700

    Java III

---
+++ JAVA/java_study_3/page12/Main.java
@@ -0,0 +1,17 @@
+// Import class Scanner dari java.util.Scanner
+import java.util.Scanner;
+
+class Main {
+  public static void main (String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.print("Nama: ");
+    
+    // Deklarasikan variable name dari tipe String untuk mengambil nilai masukkan dari console
+    String name = scanner.next();
+    
+    // Cetak "Hello, ____."
+    System.out.println("Hello, " + name + ".");
+    
+  }
+}
